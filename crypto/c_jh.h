2f908d666fd66e1eb9ec0811324f1ec5ae96e078
Дмитрий Донской <sentike161rus@gmail.com>
2018-03-26 11:55:02 +0300

    jh sse2 optimize & cleanup

--- crypto/c_jh.h
+++ crypto/c_jh.h
@@ -16,4 +16,4 @@
 
 #include "hash.h"
 
-HashReturn jh_hash(int hashbitlen, const BitSequence *data, DataLength databitlen, BitSequence *hashval);
+void jh_hash(const BitSequence *data, BitSequence *hashval);
